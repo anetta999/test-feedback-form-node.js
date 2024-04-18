4fa1a32aa609a66a641a6b660583893af93d6184
Anna Zubchenko <annazubchenko282@gmail.com>
2024-04-18 14:27:35 +0300

    added cors

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const { feedbacksRouter }= require('./routes/feedbacks')
 const notFoundHandler = require('./middlewares/notFoundHandler');
 const globalErrorHandler = require('./middlewares/globalErrorHandler');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/feedbacks', feedbacksRouter);
 
