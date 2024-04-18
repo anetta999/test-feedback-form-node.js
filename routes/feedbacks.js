9841ce01cd39083af408524c062b4817e5420503
Anna Zubchenko <annazubchenko282@gmail.com>
2024-04-18 13:43:37 +0300

    fixed feedbacks router

--- routes/feedbacks.js
+++ routes/feedbacks.js
@@ -5,7 +5,7 @@ const validationWrapper = require("../utils/validationWrapper");
 
 const router = express.Router();
 
-router.route('/feedbacks').post(validationWrapper(createFeedbackValidationSchema), createFeedback);
+router.route('/').post(validationWrapper(createFeedbackValidationSchema), createFeedback);
 
 module.exports = {
     feedbacksRouter: router,
